b7a3ee3936f54a062f9d2af0330e5dcc73b7b7a4
derrick-ho <derrickjho@protonmail.com>
2019-01-31 17:28:58 -0800

    Added Reset Button w/ resizing

--- etch.js
+++ etch.js
@@ -1,25 +1,45 @@
-const ROWS = 16;
-const COLS = 16;
-//const 
-
+const btn = document.querySelector('#reset');
 const etchasketch = document.querySelector('#etch-a-sketch');
 
-etchasketch.style.display = "flex";
-etchasketch.style.flexWrap = "wrap";
-etchasketch.style.backgroundColor = "gray";
+function askSize() {
+   var size;
 
+   do {
+      size = parseInt(prompt("Enter a number"), 10);
+   } while(isNaN(size) || size < 1);
 
-var grids = [];
-for (var i = 0; i < ROWS*COLS; i++) {
-   grids[i] = document.createElement('div');
-   grids[i].classList.add('grids');
-   etchasketch.appendChild(grids[i]);
-
-   grids[i].style.width = "60px";
-   grids[i].style.height = "60px";
-   grids[i].style.border = "thin solid black";
-   grids[i].style.flex = "1 0 6%";                   // 6% makes each grid takes 1/16 of width
-
-   grids[i].onmouseover = function() {this.style.backgroundColor = "white";};
+   return size;
 }
+
+function createSketchPad(size) {
+   etchasketch.style.display = "flex";
+   etchasketch.style.flexWrap = "wrap";
+   etchasketch.style.backgroundColor = "gray";
+   etchasketch.style.
+
+   var grids = [];
+   for (var i = 0; i < size*size; i++) {
+      grids[i] = document.createElement('div');
+      grids[i].classList.add('grids');
+      etchasketch.appendChild(grids[i]);
+
+      grids[i].style.height = "60px";
+      grids[i].style.border = "thin solid black";
+
+      var percent = 100/size - 0.25;
+      grids[i].style.flex = "1 0" + percent.toString() + "%";
+
+      grids[i].onmouseover = function() {this.style.backgroundColor = "white";};
+   }
+}
+
+function clearSketchPad() {
+   etchasketch.innerHTML = "";
+}
+
+btn.onclick = function () {
+   clearSketchPad();
+   var size = askSize();
+   createSketchPad(size);
+};
 
